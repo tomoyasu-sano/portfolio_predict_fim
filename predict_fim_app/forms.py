d779580245676a87ee2d06e13a926a20e7ea59cb
tomoyasu-sano <anytimes.sano@gmail.com>
2020-06-27 11:08:58 +0900

    edid forms.py

--- predict_fim_app/forms.py
+++ predict_fim_app/forms.py
@@ -16,7 +16,19 @@ disease=[
    (3,"廃用症候群"),
    (4,"その他")
 ]
+
 #4
+pre_hospitalization_status = （
+    (1,"自立"),
+    (2,"要支援1"),
+    (3,"要支援2"),
+    (4,"要介護1"),
+    (5,"要介護2"),
+    (6,"要介護3"),
+    (7,"要介護4"),
+    (8,"要介護5"),
+）
+
 days = (
    (1,"1週間"),
    (2,"2週間"),
@@ -28,12 +40,27 @@ days = (
    (8,"8週間"),
    (9,"9週間以上"),
 )
+family = [
+    (1,"独居"),
+    (2,"日中独居"),
+    (3,"同居"),
+    (4,"その他")
+]
 
 #5
 helper=[
    (0,"無し"),
    (1,"有り")
 ]
+
+motivation = [
+    (1,"とてもある"),
+    (2,"少しある"),
+    (3,"あまりない"),
+    (4,"全くない")
+]
+
+
 #6
 meal=[
     (1,'全介助'),
@@ -234,104 +261,122 @@ class CareForm(forms.Form):
         label='3：疾患情報', choices=disease,
         required=True,initial=[2],
         widget=forms.RadioSelect)
-
-    days = forms.ChoiceField(
-        label='４：発症からの経過日数', choices=days,
+    
+    pre_hospitalization_status = forms.ChoiceField(
+        label='４：病前の生活状態', choices=pre_hospitalization_status,
         required=True,initial=[1],
         widget=forms.Select)
 
+    days = forms.ChoiceField(
+        label='５：発症からの経過日数', choices=days,
+        required=True,initial=[1],
+        widget=forms.Select)
+
+    
+     family = forms.ChoiceField(
+        label='６：家族構成', choices=family,
+        required=True,initial=[1],
+        widget=forms.RadioSelect)
+
+
     helper = forms.ChoiceField(
-        label='５：主要介護者の有無', choices=helper,
+        label='７：主要介護者の有無', choices=helper,
+        required=True,initial=[1],
+        widget=forms.RadioSelect)
+
+        
+    motivation = forms.ChoiceField(
+        label='８：リハ意欲', choices=motivation,
         required=True,initial=[1],
         widget=forms.RadioSelect)
     
 
     meal = forms.ChoiceField(
-        label='６：食事', choices=meal,
+        label='９：食事', choices=meal,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     hygienic = forms.ChoiceField(
-        label='７：整容', choices=hygienic,
+        label='１０：整容', choices=hygienic,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     wipingClean = forms.ChoiceField(
-        label='８：清拭', choices=wipingClean,
+        label='１１：清拭', choices=wipingClean,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     upperBodyDressing = forms.ChoiceField(
-        label='９：更衣上半身', choices=upperBodyDressing,
+        label='１２：更衣上半身', choices=upperBodyDressing,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     lowerBodyDressing = forms.ChoiceField(
-        label='１０：更衣下半身', choices=lowerBodyDressing,
+        label='１３：更衣下半身', choices=lowerBodyDressing,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     toiletAction = forms.ChoiceField(
-        label='１１：トイレ動作', choices=toiletAction,
+        label='１４：トイレ動作', choices=toiletAction,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     urinationControl = forms.ChoiceField(
-        label='１２：排尿管理', choices=urinationControl,
+        label='１５：排尿管理', choices=urinationControl,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     defecationControl = forms.ChoiceField(
-        label='１３：排便管理', choices=defecationControl,
+        label='１６：排便管理', choices=defecationControl,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     bedsChairsWheelchairs = forms.ChoiceField(
-        label='１４：ベッド・椅子・車椅子移乗', choices=bedsChairsWheelchairs,
+        label='１７：ベッド・椅子・車椅子移乗', choices=bedsChairsWheelchairs,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     toilet = forms.ChoiceField(
-        label='１５：トイレ移乗', choices=toilet,
+        label='１８：トイレ移乗', choices=toilet,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     bathtubShower = forms.ChoiceField(
-        label='１６：浴槽・シャワー移乗', choices=bathtubShower,
+        label='１９：浴槽・シャワー移乗', choices=bathtubShower,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     walkingWheelchair = forms.ChoiceField(
-        label='１７：歩行・車椅子', choices=walkingWheelchair,
+        label='２０：歩行・車椅子', choices=walkingWheelchair,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     stairs = forms.ChoiceField(
-        label='１８：階段', choices=stairs,
+        label='２１：階段', choices=stairs,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     understanding = forms.ChoiceField(
-        label='１９：理解', choices=understanding,
+        label='２２：理解', choices=understanding,
         required=True,initial=[4],
         widget=forms.RadioSelect)
     
     expression = forms.ChoiceField(
-        label='２０：表出', choices=expression,
+        label='２３：表出', choices=expression,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     socialCommunication = forms.ChoiceField(
-        label='２１：社会的交流', choices=socialCommunication,
+        label='２４：社会的交流', choices=socialCommunication,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     problemSolving = forms.ChoiceField(
-        label='２２：問題解決', choices=problemSolving,
+        label='２５：問題解決', choices=problemSolving,
         required=True,initial=[4],
         widget=forms.RadioSelect)
 
     memory = forms.ChoiceField(
-        label='２３：記憶', choices=memory,
+        label='２６：記憶', choices=memory,
         required=True,initial=[4],
         widget=forms.RadioSelect)
